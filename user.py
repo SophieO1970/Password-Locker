f562608b1df4db06bf118543434d86bf6a8a2c34
sophieO1970 <sophieO1970@gmail.com>
2020-10-10 23:26:29 +0300

    add user,test,run and credentials files

---
+++ user.py
@@ -0,0 +1,27 @@
+
+
+
+# class User:
+#     '''
+#     create class that allows user to generate user account
+#     '''
+    
+#     user list = []
+    
+#     def __init__(self,user_name,password):
+        
+#         '''
+#         Method that defines the properties of a user.
+#         '''
+        
+#         self.user_name = user_name
+#         self.password = password
+        
+#     def save_user(self):
+        
+#         '''
+#         Method that saves new user instances.
+#         '''
+        
+#         User.user_list.append(self)
+        
