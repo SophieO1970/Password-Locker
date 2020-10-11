1e1cb6e0a3465cb9304a8b9060c6f571861daa68
sophieO1970 <sophieO1970@gmail.com>
2020-10-11 16:25:16 +0300

    add a save credentials function

--- run.py
+++ run.py
@@ -19,3 +19,15 @@ def save_new_user(user):
     Function to save user
     '''
     user.save_user()
+    
+def create_credential(username, social_name, account_name, password):
+    '''
+    Function to create a new user account
+    '''
+    new_credential = Credential(username, social_name, account_name, password)
+    return new_credential
+def save_credentials(credential):
+    '''
+    Function to save a new user account
+    '''
+    Credential.save_credentials(credential)    
