ddc85c6658ea89ab1bef5c77c41f32a45d6d9e71
sophieO1970 <sophieO1970@gmail.com>
2020-10-11 16:17:21 +0300

    creat a function for creating new user

--- run.py
+++ run.py
@@ -3,3 +3,12 @@ from user import user
 from credential import Credential
 import random
 import string
+
+
+
+def create_user(username,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(username,password)
+    return new_user
