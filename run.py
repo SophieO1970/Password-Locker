b4268fa3bd3387afd69149a04470b109abeb42c3
sophieO1970 <sophieO1970@gmail.com>
2020-10-11 16:51:56 +0300

    add a class method decorator

--- run.py
+++ run.py
@@ -45,3 +45,39 @@ def delete_credentials(self):
         delete_credentials method deletes account objects saved in the credential_list.
         '''
         Credential.credential_list.remove(self)    
+        
+@classmethod
+def find_by_account_name(cls,name):
+    
+        '''
+        Method that takes in a name and returns a credential that matches that name.
+
+        Args:
+            name: account_name to search for
+        Returns :
+            credential of person that matches the account_name.
+        '''
+
+        for credential in cls.credential_list:
+            if credential.account_name== name:
+                return credential        
+            
+            
+
+@classmethod
+def copy_credentials(cls, social_name):
+        '''
+        Class method that copies a credential's info after the credential's social account name is entered
+        '''
+        found_credential = cls.find_by_social_name(social_name)
+        return pyperclip.copy(found_credential.account_password)
+    
+@classmethod
+def display_credentials(cls):
+        '''
+        This method displays saved credentials
+        '''
+
+        return cls.credential_list
+
+            
